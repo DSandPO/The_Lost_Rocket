49c3bdbf85930596e3782d012408c26a248e03e7
DSandPO <56575470+DSandPO@users.noreply.github.com>
2019-10-18 15:00:21 -0700

    added 2D space kit

--- RocketGame/Assets/Swipe.cs
+++ RocketGame/Assets/Swipe.cs
@@ -2,17 +2,3 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Swipe : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
